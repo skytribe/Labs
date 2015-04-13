30064dc3fa43e53de6a530356394584c689e8b8f
skytribe <skytribe@spottysworld.com>
2015-04-13 11:18:21 -0700

    Initial Lab1 Commit

---
+++ GetTime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current Time is " + DateTime.Now.ToLocalTime());
+#if DEBUG
+            Console.ReadLine();
+#endif
+        }
+    }
+}
