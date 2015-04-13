15aca221aa5d4bc5d2e7effc9195c57fa13d5b6b
skytribe <skytribe@spottysworld.com>
2015-04-13 11:29:16 -0700

    With Comments

--- GetTime/Program.cs
+++ GetTime/Program.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 namespace GetTime
 {
+    /// <summary>
+    /// Displays the current date
+    /// </summary>
     class Program
     {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args">This is command line arguments</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Current Time is " + DateTime.Now.ToLocalTime());
+
+            // single line comment
+
+            /* Multi line
+             * Block comment
+             */
+
+
 #if DEBUG
             Console.ReadLine();
 #endif
